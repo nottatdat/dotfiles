fd51cf30569fd4a00c549d12b4c08ed46d60f8dd
tataudat <me@tatd.at>
2019-10-16 22:31:18 +0700

    scripts/path.sh: Separate related paths into a same block of codes

--- scripts/paths.sh
+++ scripts/paths.sh
@@ -2,22 +2,29 @@ export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 
 apath() {
-	new_path="$(pwd)"
-	if [ $# -eq 1 ]; then new_path=$1 fi
+    new_path="$(pwd)"
+	  if [ $# -eq 1 ]; then new_path=$1 fi
 
-	echo "export PATH=\"$new_path:"'$PATH"' >> $HOME/.scripts/paths.sh
+	  echo "export PATH=\"$new_path:"'$PATH"' >> $HOME/.scripts/paths.sh
 }
-alias epath="vim $HOME/.scripts/paths.sh"
+alias epath="nvim $HOME/.scripts/paths.sh"
 
-#export LD_LIBRARY_PATH="$HOME/frameworks/anaconda3/envs/ml/lib:$LD_LIBRARY_PATH"
-
-export GOPATH="$HOME/.go"
-
-export CUDA_HOME="/usr/local/cuda"
-
+# local binaries storage
+export PATH="$HOME/.local/bin:$PATH"
 export PATH="$HOME/bin:$PATH"
-export PATH="/usr/local/cuda/bin:$PATH"
+
+# set paths for go language
+export GOPATH="$HOME/.go"
 export PATH="$GOPATH/bin:$PATH"
+
+# set paths for CUDA binaries
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    export CUDA_HOME="/usr/local/cuda"
+    export PATH="/usr/local/cuda/bin:$PATH"
+fi
+
+# set paths for TensorFlow for Swift
 export PATH="$HOME/frameworks/tensorswift/usr/bin:$PATH"
 
 source $HOME/.ghcup/env
+
