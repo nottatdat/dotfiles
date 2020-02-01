f4cd5927bfe5d9177569407e232e53a8f88d8737
tataudat <me@tatd.at>
2020-02-02 01:49:32 +0700

    scripts/aliases.sh: Use vanilla ls

--- scripts/aliases.sh
+++ scripts/aliases.sh
@@ -170,7 +170,6 @@ alias pullo="git pull origin"
 alias pullu="git pull upstream"
 
 ## use lsd as ls alternative
-alias ls="lsd --color never"
 alias l="ls -l"
 alias la="l -a"
 alias lt="ls --tree"
