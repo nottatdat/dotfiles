949ca208be645f4a2e5e9f3ec31f5bced80c088f
tataudat <me@tatd.at>
2021-03-05 00:51:06 +0700

    xmonad: Update keybindings

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -63,7 +63,7 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     [ ((modm .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf)
 
     -- launch dmenu
-    , ((modm,               xK_p     ), spawn "dmenu_run")
+    , ((modm,               xK_p     ), spawn "rofi -combi-modi window,drun -show combi -modi combi")
 
     -- launch gmrun
     , ((modm .|. shiftMask, xK_p     ), spawn "gmrun")
