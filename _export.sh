038cb8c826bd88597343ef103b915cb6f15b644f
tataudat <me@tatd.at>
2021-07-19 01:34:22 +0700

    _export.sh: Remove unused sections

--- _export.sh
+++ _export.sh
@@ -19,36 +19,29 @@ backup_file $HOME/.zshrc
 backup_file $HOME/.gitconfig
 backup_file $HOME/.gitignore_global
 backup_file $HOME/.git-credentials
-backup_file $HOME/.config/alacritty/alacritty.yml
-backup_file $HOME/.tmux.conf
-backup_file $HOME/.Xresources
 
 backup_folder $HOME/.utils
 backup_folder $HOME/.keys
 backup_folder $HOME/.ssh
 backup_folder $HOME/.config/nvim
 backup_folder $HOME/.config/coc
-backup_folder $HOME/.xmonad
+backup_folder $HOME/.config/kitty
 
 # create symbolic links
 symlink zshrc                .zshrc
 symlink git/gitconfig        .gitconfig
 symlink git/gitignore_global .gitignore_global
 symlink git/git-credentials  .git-credentials
-symlink tmux/tmux.conf       .tmux.conf
-symlink xmonad/Xresources    .Xresources
 
 symlink utils        .utils
 symlink secrets/ssh  .ssh
 symlink secrets/keys .keys
 symlink nvim         .config/
 symlink nvim/coc     .config/
-symlink xmonad       .xmonad
+symlink kitty        .config/
 
 # handle linux related dotfiles
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-    symlink alacritty/alacritty.linux.yml .config/alacritty/alacritty.yml
-
     backup_folder $HOME/.config/i3
     mkdir $HOME/.config/i3
     symlink i3/i3_config .config/i3/config
@@ -57,11 +50,6 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
     backup_folder $HOME/.config/i3status
     mkdir $HOME/.config/i3status
     symlink i3/i3status_config .config/i3status/config
-
-    sudo cp xorg.conf /etc/X11/xorg.conf
-# handle OSX related dotfiles
-elif [[ "$OSTYPE" == "darwin" ]]; then
-    symlink alacritty/alacritty.yml .config/alacritty/alacritty.yml
 fi
 
 gpg --export-secret-key -a $GPG_UID > secrets/gpg/gpg.key
